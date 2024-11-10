63d58b2596dccf77534f345a7294a6016e44f53f
shaikazeem2001 <137382701+shaikazeem2001@users.noreply.github.com>
2024-11-09 19:05:58 -0600

    Update App.jsx
    changed projects to Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Hero from './sections/Hero/Hero';
-import Projects from './sections/projects/projects';
+import Projects from './sections/projects/Projects';
 import Skills from './sections/Skills/Skills';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
